e41e5e0e7fdbe503cc96e5862fccf708cb11858e
Hongyu Shi <44023928+HongyuS@users.noreply.github.com>
2022-06-21 23:24:26 +0800

    Initial Commit

---
+++ Tests/DequeTests/DequeTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Deque
+
+final class DequeTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Deque().text, "Hello, World!")
+    }
+}
